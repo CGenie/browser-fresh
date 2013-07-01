7505999443069329ad52fd87c3187d2d5337fdc9
Przemek Kaminski <cgenie@gmail.com>
2013-07-01 19:26:52 +0000

    Probe for server after connection lost, some requirejs simplifications

--- src/js/browser-fresh.js
+++ src/js/browser-fresh.js
@@ -2,7 +2,7 @@
  * Widget initializer to be put on a webpage.
  */
 
-var BrowserFresh = BrowserFresh || [];
+var BrowserFresh = BrowserFresh || {};
 
 // script path autodiscover
 BrowserFresh.API_HOST = BrowserFresh.API_HOST || 'http://localhost:7777/';
@@ -13,14 +13,14 @@ BrowserFresh.WS_API_HOST = BrowserFresh.API_HOST.replace("http://", "ws://");
 
 // TODO: sandbox requirejs configuration
 // require.s.contexts._.config
-BrowserFresh._setup = function() {
-    BrowserFresh.require = require.config({
+BrowserFresh.start = function(options) {
+   require({
         context: 'BrowserFresh',
         baseUrl: BrowserFresh.API_HOST + 'js/',
         paths: {
             jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min',
-	    handlers: 'handlers'
-//	    socketio: 'contrib/socket.io'
+            handlers: 'handlers'
+//          socketio: 'contrib/socket.io'
 //            text: 'contrib/text',
 //            async: 'contrib/async',
 //            moment: 'contrib/moment.min',
@@ -37,40 +37,36 @@ BrowserFresh._setup = function() {
         //         }
         //     }
         }
-    });
-}
-
-BrowserFresh.start = function(options) {
-   BrowserFresh.require(
+    },
        ['jquery', 'handlers'],
        function($, Handlers) {
-	   var socket;
+           var socket;
 
-	   var socketProbe = function() {
-	       console.log('probing for socket...');
-	       try {
-		   socket = new WebSocket(BrowserFresh.WS_API_HOST + 'ws');
-	       } catch(e) {
-		   console.log('couldn\'t find socket');
-	       }
-	       socket.onopen = function() {
-		   console.log('socket found');
-	       };
-	       socket.onmessage = function(evt) {
-		   console.log('incoming data', evt.data);
-		   var data = JSON.parse(evt.data);
-		   var handlers = Handlers.find(data.type);
-		   for(var i = 0; i < handlers.length; i++) {
-		       handlers[i].call(data.data);
-		   }
-	       }
-	       socket.onclose = function() {
-		   console.log('socket closed');
-		   setTimeout(socketProbe, 5000);
-	       }
-	   }
+           var socketProbe = function() {
+               console.log('probing for socket...');
+               try {
+                   socket = new WebSocket(BrowserFresh.WS_API_HOST + 'ws');
+               } catch(e) {
+                   console.log('couldn\'t find socket');
+               }
+               socket.onopen = function() {
+                   console.log('socket found');
+               };
+               socket.onmessage = function(evt) {
+                   console.log('incoming data', evt.data);
+                   var data = JSON.parse(evt.data);
+                   var handlers = Handlers.find(data.type);
+                   for(var i = 0; i < handlers.length; i++) {
+                       handlers[i].call(data.data);
+                   }
+               }
+               socket.onclose = function() {
+                   console.log('socket closed');
+                   setTimeout(socketProbe, 5000);
+               }
+           }
 
-	   socketProbe();
+           socketProbe();
        });
 };
 
@@ -87,10 +83,12 @@ BrowserFresh._load = function() {
             if(window.require == undefined) {
                 setTimeout(ready, 10);
             } else {
-                BrowserFresh._setup();
+                //BrowserFresh._setup();
                 BrowserFresh.start();
             }
         })();
+    } else {
+        BrowserFresh.start();
     };
 };
 BrowserFresh._load();
